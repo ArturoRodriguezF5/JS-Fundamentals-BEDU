ef85f909470d313d0e2edb7452093807e507c331
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-06-29 20:48:16 -0600

    Primer commit para subir carpeta de ejemplos de JS fundamentos.

---
+++ Sesion-01/app.js
@@ -0,0 +1,16 @@
+let email = "hola";
+
+console.log(email);
+
+email = "ey";
+
+console.log(email);
+
+console.log(typeof email);
+
+/*Ternary oprator*/
+const sig = 5;
+const op = sig < 2 ? console.log("sig es menor a 2") : console.log("sig no es menor a 2");
+
+console.log(sig);
+
