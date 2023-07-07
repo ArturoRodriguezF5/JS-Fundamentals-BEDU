d6839c7f4ab7e3f11b79c4cbbe6494cb4b39b5c6
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-07-06 19:25:08 -0600

    Commit para actualizar cambios a archivos.

---
+++ Sesion-03/Prework/app.js
@@ -0,0 +1,14 @@
+const frutas = ['mango', 'sandia', 'platano'];
+
+console.log(frutas[1]);
+
+console.log(frutas.length);
+
+const lolChamp = {
+    nombre: "Irelia",
+    edad: 23,
+    ciudad: "Freijorld"
+};
+
+console.log(lolChamp.nombre);
+console.log(lolChamp);
