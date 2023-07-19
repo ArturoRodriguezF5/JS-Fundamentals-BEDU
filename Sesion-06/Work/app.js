f6defd1887fcab85e925673865108e23bbc94be9
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-07-18 21:58:09 -0600

    Commit para subir cambios.

--- Sesion-06/Work/app.js
+++ Sesion-06/Work/app.js
@@ -5,6 +5,46 @@ const Person = function(name, lastName, age) {
     this.age = age;
 }
 
-const p1 = new Person('Arturo', 'Rodríguez', 29);
+// const p1 = new Person('Arturo', 'Rodríguez', 29);
 
-console.log(p1)
+// console.log(p1)
+
+const Medico = function(name, lastName, age, skills) {
+    Person.call(this, name, lastName, age);
+    this.skills = skills;
+    this.operar = () => console.log('Operando...');
+}
+const m1 = new Medico('Arturo', 'Rodríguez', 29, 'JS');
+console.log(m1.age)
+console.log(m1)
+
+const Group = function(g) {
+    this.g = [g];
+
+    this.add = (v) => {
+        if(! list.includes(v)) {
+            list.push(v);
+        }
+    }
+
+    this.has = (v) => {
+        return list.includes(v);
+    }
+     
+    this.from = (group) => {
+        g = group;
+    }
+}
+
+const Triangulo = function(base, altura) {
+    this.base = base;
+    this.altura = altura;
+}
+
+Triangulo.prototype.calculaArea = function() {
+    return this.base * this.altura / 2;
+}
+
+const t1 = new Triangulo(4, 6);
+
+console.log(t1.calculaArea())
