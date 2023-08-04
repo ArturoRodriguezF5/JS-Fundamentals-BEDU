be1805dd0c1179bc2b2b06e7044ef13ce8e12c0e
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-08-03 21:49:05 -0600

    Commit para subir cambios de sesion11

---
+++ Sesion-11/Work/anidarPromesas.js
@@ -0,0 +1,33 @@
+function addOne(valor) {
+    return new Promise(() => {
+        setTimeout((resolve) => {
+            valor += 1;
+        }, 500)
+    })
+}
+
+addOne(1)
+    .then((x) => {
+        console.log(x)
+        return addOne(x)
+    })
+    .then(x => {
+        console.log(x)
+        return addOne(x)
+    })
+
+// Async / Await
+
+async function fooA() {
+    return "Hola Async Await"
+}
+
+fooA()
+    .then(x => console.log(x))
+
+async function bar() {
+    let saludo = await fooA()
+    console.log(saludo)
+}
+
+bar()
