5af806e88cea584b90df31a32e1a44fc25059872
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-08-10 19:11:25 -0600

    Commit para subir cambios de sesion12

---
+++ Sesion-12/Work/ejemplo2/app.js
@@ -0,0 +1,41 @@
+// const input = document.querySelector("#search");
+// const button = document.querySelector("#button");
+// const results = document.querySelector("#results");
+
+// function searchGifs() {
+//     const search = input.value;
+
+//     if(search) {
+//         const url = buildUrl(search)
+
+//         getGiphyResults(url)
+//         .then(function(gifs) {
+//             console.log(gifs)
+
+//             gifs.forEach(function(gif) {
+//                 const img = document.createElement("img")
+//             })
+//         })
+//     }
+// }
+
+// function getGiphyResults (url) {
+//     return fetch(url)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function() {
+//         return data.data;
+//     })
+// }
+
+
+
+
+
+console.log("hola")
+
+
+let n = 2 + '5';
+console.log(n)
+console.log()
