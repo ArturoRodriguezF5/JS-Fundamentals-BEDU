ad01496d10487908e342f114c1bf19fec589be7e
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-08-12 23:14:23 -0600

    Commit para actualizar cambios en ejercicios.

--- Sesion-10/Work/app.js
+++ Sesion-10/Work/app.js
@@ -1,1 +1,16 @@
-console.log("hola")
+const frutas = [
+    "Sandia",
+    "Limon",
+    "Pepino",
+    "Tuna"
+];
+
+const myFunc = (frutas) => {
+    return frutas.map(x => {
+        return `La: ${x}`;
+    });
+};
+
+const arrF = myFunc(frutas);
+
+arrF.forEach(x => console.log(x));
